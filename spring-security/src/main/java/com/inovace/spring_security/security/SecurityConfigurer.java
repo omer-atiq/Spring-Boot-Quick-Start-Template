7247c62d9a16119055605534e4dca9ffb463e151
omer atiq <omer.attique@gmail.com>
2025-09-21 15:17:30 +0500

    Salt hasded Password loginc with Bcrypt Encoder added

--- spring-security/src/main/java/com/inovace/spring_security/security/SecurityConfigurer.java
+++ spring-security/src/main/java/com/inovace/spring_security/security/SecurityConfigurer.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -49,7 +50,8 @@ public class SecurityConfigurer {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+//         return NoOpPasswordEncoder.getInstance();
+         return new BCryptPasswordEncoder(); // default str
     }
 
 }
--- spring-security/src/main/java/com/inovace/spring_security/services/UserService.java
+++ spring-security/src/main/java/com/inovace/spring_security/services/UserService.java
@@ -3,6 +3,7 @@ package com.inovace.spring_security.services;
 import com.inovace.spring_security.entities.User;
 import com.inovace.spring_security.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +12,14 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User createUser(String username, String password,String roles,Boolean active) {
-        User user = new User(username, password,roles,active);
+
+        String hashedPassword = passwordEncoder.encode(password);
+
+        User user = new User(username, hashedPassword,roles,active);
         return userRepository.save(user);
     }
 }
